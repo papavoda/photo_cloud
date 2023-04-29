8a9dee1da8eb28e03242b5f4863e2a777a485505
papavoda <papavoda@gmail.com>
2023-04-29 11:55:12 +0300

    template update 29.04.2023

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import HomeView
+from .views import HomeView, CategoryListView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('category/', CategoryListView.as_view(), name="category_list"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from django.views.generic.base import TemplateView
-
+from .models import Category
 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        return context
+
+
+class CategoryListView(ListView):
+    model = Category
+    num_categories = Category.objects.count()
+    template_name = 'blog/category_list.html'
+    list_categories = Category.objects.all()
+    queryset = Category.objects.all()
+    context_object_name = 'my_category_list'
+
+    def get_context_data(self, **kwargs):
+        # В первую очередь получаем базовую реализацию контекста
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        # Добавляем новую переменную к контексту и инициализируем её некоторым значением
+        context['num_cat'] = self.num_categories
+        context['url'] = 'category/'
         return context
